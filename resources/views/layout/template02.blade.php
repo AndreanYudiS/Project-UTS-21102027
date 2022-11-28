77ffa9d87d612a16644323f7d8127623e6bd1ad4
AndreanYudiS <yudicarreo93@gmail.com>
2022-11-28 08:23:38 +0800

    frist commit

---
+++ resources/views/layout/template02.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('Trying')</title>
+    <link rel= "stylesheet" href="css/new3.css">
+    
+
+</head>
+<body>
+	</div>
+    <div class="container m">
+        <ul>
+        <li><a href="/uts1/public/syntax"><h3>Code</h3></a></li>
+            <li><a href="/uts1/public/alat"><h3>Komponent</h3></a></li>
+            <li><a href="/uts1/public/project"><h3>Project</h3></a></li>
+            <li><a href="/uts1/public"><h3>Beranda</h3></a></li>
+            <div class="logo">
+                <h1>Sensor and Transduser</h1>
+            </div>
+        </ul>
+    </div>
+    <style></style>
+    <div class="juduL">
+                <h4>KOMPONEN YANG DIBUTUHKAN</h4>
+            </div>
+    <div class="container footer">
+    <small>Copyright &copy; 2022 - ANDREYUDI. All Rights Reserved.</small>
+    </div>
+
+    <div class="Arduino">
+        <img src="img/Arduino_uno_prev_ui.png">
+    </div>
+    <h4>SPESIFIKASI :</h4>
+    <p>1. Microcontroller ATmega328P</p>
+    <p>2. Operating Voltage 5V</p>
+    <p>3. Digital I/O Pins 14</p>
+    <p>4. Analog Input Pins 6</p>
+    <p>5. Clock Speed 16 MHz</p>
+    <p>6. LED_BUILTIN 13</p> 
+
+    <div class="Arduino">
+    <img src="img/breadboard.png">
+</div>
+    <h4>SPESIFIKASI :</h4>
+    <p>1. Terminal Strip</p>
+    <p>2. Tie-point 400</p>
+    <p>3. ABS plastic material</p>
+    <p>4. Dimensions: 81mm x 55mm x 9mm</p>
+
+    <div class="Arduino">
+    <img src="img/kabel jumper.png">
+    </div>
+    <h4>SPESIFIKASI :</h4>
+    <p>1. Panjang: +- 20cm</p>
+    <p>2. Tipe Female to Femmale / Male to Male / Male to Female</p>
+    <p>3. Ukuran pitch konektor : 2.54mm</p>
+    <p>4. Ujung kawat kaku dengan isi kabel lemas</p>
+    
+    <div class="SERVO">
+    <img src="img/SERVO.png">
+    </div>
+    <h4>SPESIFIKASI :</h4>
+    <p>1. Ukuran: 21.5mm x 11.8mm x 22.7mm</p>
+    <p>2. Stall torque: 1.2 - 1.4 kg / cm (4.8V)</p>
+    <p>3. Voltase : 4.8V-6V</p>
+    <p>4. Suhu operasi: -30 to +60 derajat Celsius</p>
+    
+    <div class="Arduino">
+    <img src="img/Buzzer.png">
+    </div>
+    <h4>SPESIFIKASI :</h4>
+    <p>1. Tegangan kerja: 4v-8v DC (optimal 5v)</p>
+    <p>2. Kekuatan suara max: 85dB / 10cm</p>
+    <p>3. Suhu kerja: -20 ~ +70 C</p>
+    <p>4.  Arus max: 30mA / 5vDC</p>
+
+    <div class="Arduino">
+    <img src="img/Balltilt.png">
+    </div>
+    <h4>SPESIFIKASI :</h4>
+    <p>1. Operating Ratings Vcc Range : 3.3-5V</p>
+    <p>2. Ampere 	: 20mA</p>
+    <p>3. Size		: 18,5mm X 15mm</p>
+    <p>4. Suhu Kerja 	: -10℃+50℃</p>
+
+    <div class="Arduino">
+    <img src="img/LED.png">
+    </div>
+    <h4>SPESIFIKASI :</h4>
+    <p>1. Tegangan 		: 2v</p>
+    <p>2. Ukuran 		: 5mm</p>
+    <p>3. Pin 		: Pin Anoda (+) dan Pin Katoda (-)</p>
+
+</body>
+</html>
---
+++ resources/views/layout/template05.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('Trying')</title>
+    <link rel= "stylesheet" href="css/new4.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    
+
+</head>
+
+<body>
+ <!-- loader -->
+ <div class="bg-loader">
+  <div class="loader"></div>
+ </div>
+ <!-- header -->
+ <div class="medsos">
+  <div class="container">
+   <ul>
+    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+   </ul>
+  </div>
+ </div>
+ <header>
+  <div class="container">
+   <h1><a href="index.html">Gede Andrean Yudi Saputra</a></h1>
+   <ul>
+   <li><a href="/uts1/public/syntax"><h3>Code</h3></a></li>
+            <li><a href="/uts1/public/alat"><h3>Komponent</h3></a></li>
+            <li><a href="/uts1/public/project"><h3>Project</h3></a></li>
+            <li><a href="/uts1/public"><h3>Beranda</h3></a></li>
+   </ul>
+  </div>
+ </header>
+ <!-- banner -->
+ <section class="banner">
+  <h2>Hiii..</h2>
+ </section>
+ <!-- about -->
+ <section class="about">
+  <div class="container">
+   <h3>ABOUT</h3>
+   <p>UTS WEB PROGRAMING<strong>MUDAH MUDAHAN LANCAR</strong>Terimakasih</p>
+  </div>
+ </section>
+ <!-- service -->
+ <section class="service">
+  <div class="container">
+   <h3>SERVICE</h3>
+   <div class="box">
+    <div class="col-4">
+     <div class="icon"><i class="fas fa-mobile"></i></div>
+     <h4>MOBILE APP</h4>
+    </div>
+    <div class="col-4">
+     <div class="icon"><i class="fas fa-globe"></i></div>
+     <h4>WEB DEVELOPMENT</h4>
+    </div>
+    <div class="col-4">
+     <div class="icon"><i class="fas fa-edit"></i></div>
+     <h4>DESIGN</h4>
+    </div>
+    <div class="col-4">
+     <div class="icon"><i class="fas fa-chart-bar"></i></div>
+     <h4>DIGITAL MARKETING</h4>
+    </div>
+   </div>
+  </div>
+ </section>
+ <!-- footer -->
+ <footer>
+  <div class="container">
+   <small>Copyright &copy; 2020 - Dzulfikar. All Rights Reserved.</small>
+  </div>
+ </footer>
+ <script type="text/javascript">
+  $(document).ready(function(){
+   $(".bg-loader").hide();
+  })
+  </script>
+  </body>
+</html>
